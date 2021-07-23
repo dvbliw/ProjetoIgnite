6f2b3623e3ed4bf54b09d4ab4aaa17ec39a8d5d6
Walef Xavier <dvbliw@gmail.com>
2021-07-23 15:57:52 -0300

    DTMONEY

---
+++ DTMONEY/src/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createServer, Model } from "miragejs";
+import { App } from "./App";
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Projeto",
+          type: "deposit",
+          category: "Design",
+          amount: 2000,
+          createdAt: new Date("2021-06-11 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Viagem",
+          type: "withdraw",
+          category: "Despesa",
+          amount: 1300,
+          createdAt: new Date("2021-06-11 11:00:00"),
+        },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transaction", data);
+    });
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
